189b5f32014e67674369318e66d0941a8128afe6
Felipe Gomes <felipegstech@gmail.com>
2025-08-22 02:02:00 -0300

    feat: step navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,12 @@ const btNext = document.querySelector("#btNext");
 const inName = document.querySelector("#inName");
 const inEmail = document.querySelector("#inEmail");
 const inNumber = document.querySelector("#inNumber");
-const frm = document.querySelector("form");
+const frmInfo = document.querySelector("#formInfo");
 
-const numbers = document.querySelectorAll(".container-number")
+function goToStep(current, next) {
+    current.classList.remove("activated");
+    next.classList.add("activated")
+}
 
 let user = {
     "userName": "",
@@ -15,8 +18,8 @@ let user = {
 const step1 = document.querySelector(".step-1")
 const step2 = document.querySelector(".step-2")
 
-frm.addEventListener("submit", (e) => {
-    e.preventDefault()
+frmInfo.addEventListener("submit", (e) => {
+    e.preventDefault();
 
     user.userName = inName.value;
     user.userEmail = inEmail.value;
@@ -24,7 +27,38 @@ frm.addEventListener("submit", (e) => {
 
     console.log(user)
 
-    step1.classList.remove("activated")
-    step2.classList.add("activated")
+    goToStep(step1, step2)
+    nextNumber()
 })
 
+const frmPlan = document.querySelector("#formPlan");
+const btBack = document.querySelector(".btBack");
+
+btBack.addEventListener("click", () => {
+    goToStep(step2, step1)
+    prevNumber()
+})
+
+const steps = document.querySelectorAll(".step");
+const numbers = document.querySelectorAll(".container-number");
+let currentStep = 0;
+
+function showSteps(index) {
+    steps.forEach((step, i) => {
+        step.classList.toggle("activated", i === index);
+    })
+
+    numbers.forEach((number, i) => {
+        number.classList.toggle("numberActivated", i === index);
+    });
+}
+
+function nextNumber() {
+    currentStep++;
+    showSteps(currentStep);
+}
+
+function prevNumber() {
+    currentStep--;
+    showSteps(currentStep);
+}
